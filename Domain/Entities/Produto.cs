0a52c9576f43e7409b6590b027dadae4b9c11493
Tarde Cetafaju <Tarde.Cetafaju@labfies.org.br>
2024-03-11 16:44:21 -0300

    first commit

---
+++ Domain/Entities/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Produto : BaseEntity
+    {
+        public string Desciption { get; set; }
+        public string Price { get; set;}
+        public int Stock { get; set;}
+        public int MinStock { get; set; }
+    }
+}
